33aea1ff46b84a84bb2a52338b982747c9c1d8d7
asdiop153246 <magicsiam001@gmail.com>
2022-11-23 09:42:38 +0700

    HP/Takedamage/Heal

---
+++ Assets/Script/Character/PlayerHealth.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerHealth : MonoBehaviour
+{
+    private float health;
+    private float lerptimer;
+    public float maxHealth = 100f;
+    public float chipSpeed = 2f;
+    public Image frontHealthBar;
+    public Image backHealthBar;
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = Mathf.Clamp(health, 0, maxHealth);
+        UpdateHealthUI();
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            TakeDamage(Random.Range(5, 10));
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            RestoreHealth(Random.Range(5, 10));
+        }
+    }
+
+    public void UpdateHealthUI()
+    {
+        Debug.Log(health);
+        float fillF = frontHealthBar.fillAmount;
+        float fillB = backHealthBar.fillAmount;
+        float hFraction = health / maxHealth;
+        if(fillB > hFraction)
+        {
+            frontHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.red;
+            lerptimer += Time.deltaTime;
+            float percentComplete = lerptimer / chipSpeed;
+            backHealthBar.fillAmount = Mathf.Lerp(fillB, hFraction, percentComplete);
+        }
+        if (fillF < hFraction)
+        {
+            frontHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.green;
+            lerptimer += Time.deltaTime;
+            float percentComplete = lerptimer / chipSpeed;
+            backHealthBar.fillAmount = Mathf.Lerp(fillF, hFraction, percentComplete);
+        }
+    }
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        lerptimer = 0f;
+
+    }
+    public void RestoreHealth(float healamount)
+    {
+        health += healamount;
+        lerptimer = 0f;
+    }
+}
